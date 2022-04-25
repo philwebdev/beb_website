26dae1fa0c12e9e02fa4104f1913faf01a7b9800
Filippo Papagno <f.papagno94@gmail.com>
2022-04-25 22:36:17 +0200

    Prime lavorazioni

---
+++ BEBWeb/src/app/stanze/stanze.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stanze',
+  templateUrl: './stanze.component.html',
+  styleUrls: ['./stanze.component.scss']
+})
+export class StanzeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
